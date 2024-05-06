f0078169639226c4ff8115a6e068584c457ad023
UBLDesigns <hello@derby-web-design-agency.co.uk>
2024-05-06 10:59:46 +0100

    404 Pages

---
+++ error.vue
@@ -0,0 +1,14 @@
+<template>
+    <NuxtLayout>
+        <section class="text-center">
+            <h1 class="text-3xl lg:text-8xl font-bold">Error: {{ error.statusCode }}</h1>
+            <h2 class="mt-4 mb-8 text-xl lg:text-3xl">{{ error.message }}</h2>
+        </section>
+    </NuxtLayout>
+</template>
+
+<script setup>
+defineProps({
+    error: Object
+})
+</script>
--- pages/posts/[...slug].vue
+++ pages/posts/[...slug].vue
@@ -1,19 +1,25 @@
 <template>
     <article class="prose prose-pre:border prose-pre:border-black/10 prose-pre:bg-white prose-pre:text-gray-700 dark:prose-invert dark:prose-pre:bg-gray-950 dark:prose-pre:text-gray-200 max-w-none">
-        <ContentDoc v-slot="{ doc }">
-            <div class="grid grid-cols-6 gap-16 w-full">
-                <div :class="{'col-span-6 order-last lg:order-first lg:col-span-4' : doc.enableNav, 'col-span-6' : !doc.enableNav }">
-                    <ContentRenderer :value="doc" />
+        <ContentDoc>
+            <template #not-found>
+                <h1>Error: 404</h1>
+                <p>Sorry the page your are looking for has not been found</p>
+            </template>
+            <template v-slot="{ doc }">
+                <div class="grid grid-cols-6 gap-16 w-full">
+                    <div :class="{'col-span-6 order-last lg:order-first lg:col-span-4' : doc.enableNav, 'col-span-6' : !doc.enableNav }">
+                        <ContentRenderer :value="doc" />
+                    </div>
+                    <div v-if="doc.enableNav" class="col-span-6 order-first lg:col-span-2 border-none lg:border-s border-gray-200 dark:border-gray-700 px-0 lg:px-6 lg:order-last">
+                        <aside class="sticky top-8 lg:top-40">
+                            <div class="font-semibold text-lg mb-2">Table Of Contents</div>
+                            <nav class="text-md lg:text-sm">
+                                <TOCLinks :links="doc.body.toc.links" :active-id="activeId" />
+                            </nav>
+                        </aside>
+                    </div>
                 </div>
-                <div v-if="doc.enableNav" class="col-span-6 order-first lg:col-span-2 border-none lg:border-s border-gray-200 dark:border-gray-700 px-0 lg:px-6 lg:order-last">
-                    <aside class="sticky top-8 lg:top-40">
-                        <div class="font-semibold text-lg mb-2">Table Of Contents</div>
-                        <nav class="text-md lg:text-sm">
-                            <TOCLinks :links="doc.body.toc.links" :active-id="activeId" />
-                        </nav>
-                    </aside>
-                </div>
-            </div>
+            </template>
         </ContentDoc>
     </article>
 </template>
